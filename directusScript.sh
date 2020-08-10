1885756517e34bb508f0c86df2db459c52e53b27
MAXPOL <maxrol32@gmail.com>
2020-08-10 23:36:10 +0300

    Update directusScript.sh

--- directusScript.sh
+++ directusScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get install -y apache2 mariadb-server php php-gd php-curl php-mbstring php-mysql php-xml php-pdo php-fileinfo php-mysql nano wget zip unzip composer
 systemctl enable apache2
-systemctl enable mariadb
+#systemctl enable mariadb #Uncomment if you want use internal database
 systemctl start apache2
-systemctl start mariadb
+#systemctl start mariadb #Uncomment if you want use internal database
 a2dissite 000-default.conf
 cp /directus/directus/directus.conf /etc/apache2/sites-available/
 echo "Include "/etc/apache2/sites-available/directus.conf"" >> /etc/apache2/apache2.conf
@@ -18,9 +18,9 @@ composer install
 cd /var/www/directus/migrations/install
 sed -i 's/'"'limit'"' => 255,/'"'limit'"' => 190,/g' 20180220023152_create_collections_presets_table.php
 cd /
-mysql_secure_installation
-mysql -u root -p -e "use mysql; update user set plugin='' where user='root';"
-mysql -u root -p -e "create database sdb;"
+#mysql_secure_installation #Uncomment if you want use internal database
+#mysql -u root -p -e "use mysql; update user set plugin='' where user='root';" #Uncomment if you want use internal database
+#mysql -u root -p -e "create database sdb;" #Uncomment if you want use internal database
 chmod -R 0777 /var/www/directus
 chown -R www-data:www-data /var/www/directus
 systemctl restart mariadb
